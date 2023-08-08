e1c1e30139325d31856304e9df0aade71c2c98e0
jezr250 <jezr250sl@gmail.com>
2023-08-09 05:25:27 +0900

    GooglePlayTest1

--- app/src/main/java/io/github/jezr250/mahjong2/MainActivity.java
+++ app/src/main/java/io/github/jezr250/mahjong2/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.net.Uri;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,7 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    public void openPrivacyPolicy(View view) {
+        // ここにプライバシーポリシーへのリンク先の処理を記述します
+        // 例えば、ブラウザを開いてプライバシーポリシーページに移動する場合:
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://jezr250.github.io/mjPrivacyPolicy/src/"));
+        startActivity(intent);
+    }
     public void onStartButtonClicked(View view) {
         Intent intent = new Intent(this, YakuActivity.class);
         startActivity(intent);
